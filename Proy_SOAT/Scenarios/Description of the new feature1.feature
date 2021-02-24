93ed622dcc9bd139f05f4d54016fbbcecc76c9b2
lhuyz87@gmail.com <Lhuyz666>
2021-02-23 22:13:51 -0500

    Feature consulta

--- Proy_SOAT/Scenarios/Description of the new feature1.feature
+++ Proy_SOAT/Scenarios/Description of the new feature1.feature
@@ -1,6 +1,14 @@
 ﻿Feature: Consulta de estado de cuenta
 
-  Scenario: A description of your business scenario
-    Given some precondition
-    When an action is performed
-    Then validate a condition
+  Scenario Outline: Relizar consulta de estado de cuenta por DNI
+    Given Acceso al sistema portal de corredores
+    And Ingreso a la opción Mi Portafolio con el corredor usuario "<usuario>"  y contraseña "<password>"
+    And Seleccionamos la opción Estado de cuenta por cuotas
+    When ingresamos el DNI "<documento>" y seleccionamos opción buscar
+    Then validamos que se muestre información
+    And cerramos sesion
+    
+    Examples:
+    
+    |usuario|password|documento|
+    |vparvina@everis.com|Abc.123|51078135|
--- Proy_SOAT/Script/StepDefinitions.js
+++ Proy_SOAT/Script/StepDefinitions.js
@@ -1,1 +1,46 @@
-﻿
+﻿Given("Acceso al sistema portal de corredores", function (){
+
+KeywordTests.OpenBrowser.Run();
+
+  
+
+});
+
+
+
+Given("Ingreso a la opción Mi Portafolio con el corredor usuario {arg}  y contraseña {arg}", function (param1, param2){
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/").FindElement("a[aria-labelledby='layout_20']").Click();
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/").FindElement("[role='presentation']:nth-child(1) > [href='#']").Click();
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/").FindElement("*[name='_com_liferay_login_web_portlet_LoginPortlet_login']").SetText(param1);
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/").FindElement("input[type='password']").SetText(param2);
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/").FindElement(".lfr-btn-label").Click();
+
+});
+
+Given("Seleccionamos la opción Estado de cuenta por cuotas", function (){
+  Delay(3000);
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/group/guest/pga/incentivos").FindElement("//a[contains(text(), 'Estados de Cuenta')]").Click();
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/group/guest/pga/incentivos").FindElement("//a[contains(., 'Cuotas')]").Click();
+});
+
+When("ingresamos el DNI {arg} y seleccionamos opción buscar", function (param1){
+  Delay(3000);
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/group/guest/pga/cuotas").FindElement("//strong[contains(text(), 'Nº Póliza / Contrato')]").Click();
+  Delay(2000);
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/group/guest/pga/cuotas").FindElement("//li[text()='DNI']").Click();
+  Delay(2000);
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/group/guest/pga/cuotas").FindElement("#inputBuscador").SetText(param1);
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/group/guest/pga/cuotas").FindElement("#busqueda").Click();
+  
+});
+
+Then("validamos que se muestre información", function (){
+  Delay(3000);
+  
+  aqObject.CheckProperty(Aliases.browser.pageCuotasSomosCorredoresDePacFi.FindElement("//div[2]/div/table/tbody/tr/td"), "contentText", cmpNotContains, "No se encontraron registros");
+});
+
+Then("cerramos sesion", function (){
+  Aliases.browser.pageCuotasSomosCorredoresDePacFi.FindElement(".open-slide > [data-senna-off='true']").Click();
+  Aliases.browser.Page("https://somoscorredorescrt.pacificotest.com.pe/group/guest/pga/cuotas#").FindElement("//a[text()='Salir de mi cuenta']").Click();
+});
